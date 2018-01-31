6e3a249220faee2000a1d043fa89af9c6c4c7e7d
NikitaDaffern <b6029665@my.shu.ac.uk>
2018-01-31 12:30:31 +0000

    Created data for module and lecturer controllers

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -41,6 +41,44 @@ class BootStrap {
        studyMode: 'FullTime',
        tuitionFees: '9000.00',
        description: 'LOREM IPSUM').save()
+
+       def student1=new Student(
+       name: 'Nikita Daffern',
+       studentID: '26029665',
+       dob: new Date('11/09/1997'),
+       email: 'b6029665@my.shu.ac.uk',
+       username: 'b6029665',
+       password: 'dhuiwy733u8d',
+       isFundingAvailable: 'true',
+       course: 'Computing').save()
+
+       def student2=new Student(
+       name: 'Tatjana Cerniha',
+       studentID: '24028765',
+       dob: new Date('11/09/1994'),
+       email: 'b4028765@my.shu.ac.uk',
+       username: 'b4028765',
+       password: 'hdk01234209hu',
+       isFundingAvailable: 'true',
+       course: 'Computing').save()
+
+
+       def databases=new Module(
+       title: 'Databases',
+       code: 'DB123',
+       credits: '40',
+       lecturer: 'Lynne Dawson',
+       course: 'Computing',
+       description: 'lorem ipsum').save()
+
+       def lecturer1=new Lecturer(
+       fullName: 'Lynne Dawson',
+       post: 'Lecturer',
+       subject: 'Databases',
+       lecturerEmail: 'lynnedawson@shu.ac.uk',
+       office: '9407',
+       bio: 'Databases lecturer').save()
+
 }
     def destroy = {
     }
